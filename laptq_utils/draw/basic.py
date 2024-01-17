0244539d09b85cd22e446d618e09d8d2772945e4
ProgrammingForFun <lap.tq4@gmail.com>
2024-01-17 22:25:24 +0700

    refine basic opencv draw functions

---
+++ laptq_utils/draw/basic.py
@@ -0,0 +1,64 @@
+import cv2
+
+
+COLORS = [
+    # RED, YELLO, BLUE, GREEN, PUPLE
+    (83, 50, 250), (55, 250, 250), (255, 221, 21), (102, 255, 102), (240, 120, 240),
+    (83, 179, 36), (240, 120, 240), (51, 153, 204), (187, 125, 250), (51, 204, 255),
+    (80, 83, 239), (59, 235, 255), (247, 195, 79), (132, 199, 129), (200, 104, 186),
+    (53, 57, 229), (45, 192, 251), (229, 155, 3), (71, 160, 67), (176, 39, 156),
+]
+
+
+def cv2_imshow(
+        img,
+        **kwargs
+):
+    window_name = kwargs.get('window_name', 'show')
+    flag = kwargs.get('flag', cv2.WINDOW_AUTOSIZE)
+    wait = kwargs.get('wait', 0)
+
+    cv2.namedWindow(window_name, flag)
+    cv2.imshow(window_name, img)
+    key = cv2.waitKey(wait)
+    return key
+
+
+def cv2_putText(
+        img,
+        text,
+        org,
+        **kwargs
+):
+    fontFace = kwargs.get('fontFace', cv2.FONT_HERSHEY_SIMPLEX)
+    fontScale = kwargs.get('fontScale', 1)
+    color = kwargs.get('color', (255, 255, 255))
+    thickness = kwargs.get('thickness', 1)
+    lineType = kwargs.get('lineType', cv2.LINE_AA)
+
+    x1, y1 = org
+    b, g, r = color
+    cv2.putText(img, text, (x1 + 2, y1 + 2), fontFace, fontScale, (0, 0, 0), thickness, lineType)
+    cv2.putText(img, text, (x1 + 1, y1 + 1), fontFace, fontScale, (b//2, g//2, r//2), thickness, lineType)
+    cv2.putText(img, text, (x1 + 1, y1), fontFace, fontScale, (b//2, g//2, r//2), thickness, lineType)
+    cv2.putText(img, text, (x1, y1 + 1), fontFace, fontScale, (b//2, g//2, r//2), thickness, lineType)
+    cv2.putText(img, text, org, fontFace, fontScale, color, thickness, lineType)
+
+
+def cv2_rectangle(
+        img,
+        pt1,
+        pt2,
+        **kwargs
+):
+    color = kwargs.get('color', (255, 255, 255))
+    thickness = kwargs.get('thickness', 1)
+    lineType = kwargs.get('lineType', cv2.LINE_AA)
+
+    x1, y1 = pt1
+    x2, y2 = pt2
+    b, g, r = color
+    cv2.rectangle(img, (x1 + 2, y1 + 2), (x2 + 2, y2 + 2), (0, 0, 0), thickness, lineType)
+    cv2.rectangle(img, (x1 + 1, y1 + 1), (x2 + 1, y2 + 1), (b//2, g//2, r//2), thickness, lineType)
+    cv2.rectangle(img, pt1, pt2, color, thickness, lineType)
+
---
+++ tests/test_draw.py
@@ -0,0 +1,49 @@
+from pathlib import Path
+import sys
+
+HERE = Path(__file__).parent
+ROOT_DIR = HERE.parent
+sys.path.append(str(ROOT_DIR))
+
+from laptq_utils import cv2_imshow, cv2_putText, cv2_rectangle
+
+# ==============================================================================
+
+import pytest
+import numpy as np
+
+
+def test_cv2_imshow():
+    img = 255 + np.zeros((256, 256, 3), dtype=np.uint8)
+    key = cv2_imshow(img)
+    assert chr(key).strip().lower() == 't'
+
+
+@pytest.mark.parametrize(
+        'imgsz,text,org,fontScale,color,thickness',
+        [
+            (256, 'Yellow text', (10, 128), 1, (0, 255, 255), 1),
+            (256, 'Bigger text', (10, 128), 2, (0, 255, 255), 1),
+            (256, 'Thicker text', (10, 128), 1, (0, 255, 255), 2),
+        ]
+)
+def test_cv2_putText(
+        imgsz,
+        text,
+        org,
+        fontScale,
+        color,
+        thickness,
+):
+    img = 255 + np.zeros((imgsz, imgsz, 3), dtype=np.uint8)
+    cv2_putText(img, text, org, fontScale=fontScale, color=color, thickness=thickness)
+    key = cv2_imshow(img)
+    assert chr(key).strip().lower() == 't'
+    
+    
+
+
+if __name__ == '__main__':
+    pytest.main([__file__])
+
+
