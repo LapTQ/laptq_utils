37ad06a01555bb7dc0400b38048ee2e4e2ad6e66
ProgrammingForFun <lap.tq4@gmail.com>
2024-01-24 16:32:24 +0000

    msg

--- laptq_utils/mics/check.py
+++ laptq_utils/mics/check.py
@@ -1,12 +1,11 @@
 import logging
-import numpy as np
-import torch
 
 
 logger = logging.getLogger(__name__)
 
 
 def get_device(device=None):
+    import torch
     if device is None or device == 'auto':
         device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         logger.info('Device was not specified, while cuda is {}. Using {}.'.format(
