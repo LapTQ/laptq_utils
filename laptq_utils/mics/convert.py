87ba3bba1380a7665135c39e2ab9b7141bee6cf0
LapTQ <lap.tq4@gmail.com>
2024-03-28 23:27:13 +0700

    msg

--- laptq_utils/mics/convert.py
+++ laptq_utils/mics/convert.py
@@ -2,6 +2,7 @@ import base64
 from io import BytesIO
 from PIL import Image
 import numpy as np
+import pickle
 
 
 def np_to_b64(
@@ -21,4 +22,19 @@ def b64_to_np(
     img_b64 = kwargs['img_b64']
     img = np.array(Image.open(BytesIO(base64.b64decode(img_b64))))
     return img
-    
+
+
+def obj_to_pklstr(
+        **kwargs
+):
+    obj = kwargs['obj']
+    pkl_str = pickle.dumps(obj).decode('latin1')
+    return pkl_str
+
+
+def pklstr_to_obj(
+        **kwargs
+):
+    pkl_str = kwargs['pkl_str']
+    obj = pickle.loads(pkl_str.encode('latin1'))
+    return obj
