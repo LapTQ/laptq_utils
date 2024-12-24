bf0402e2a15db5efd918af8d8c39dc677a942033
LapTQ <lap.tq4@gmail.com>
2024-12-24 21:39:56 +0700

    msg

---
+++ scripts/create-env.sh
@@ -0,0 +1,9 @@
+venv_parent=/media/laptq/data/workspace/laptq_utils
+venv_name=.venv
+venv_path=$venv_parent/$venv_name
+[[ ! -d $venv_path ]] && python3 -m venv $venv_path
+source $venv_path/bin/activate
+which python3
+
+
+# pip install -r --ignore-installed requirements.txt --default-timeout=10000
