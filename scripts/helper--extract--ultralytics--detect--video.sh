46dfaf57332c0cc1dec28563165cabedb1d22c51
LapTQ <lap.tq4@gmail.com>
2024-12-30 09:46:33 +0700

    msg

--- scripts/helper--extract--ultralytics--detect--video.sh
+++ scripts/helper--extract--ultralytics--detect--video.sh
@@ -1,12 +1,16 @@
 PATH__DIR__VIDEO=/home/laptq/laptq-prj-21/data/Videos/241210_受け取り動画/mp4_5min
 
 # PATH__FILE__MODEL=/home/laptq/laptq-prj-21/runs/data--public--satudora/yolo11s--832--scale-0.5--multiscale-True/train2/weights/best.pt
-PATH__FILE__MODEL=/mnt/ssd8tb/shared_workspace/manhpc/FS_prj21/runs/train_RAF_val_RAF/weights/best.pt
+# PATH__FILE__MODEL=/mnt/ssd8tb/shared_workspace/manhpc/FS_prj21/runs/train_RAF_val_RAF/weights/best.pt
+PATH__FILE__MODEL=/home/laptq/laptq-prj-21/runs/data--synthetic--satudora-center-box/yolo11s--832--scale-0.5--multiscale-True/train/weights/best.pt
 
 # PATH__DIR__IMAGE__OUTPUT=/home/laptq/laptq-prj-21/runs/data--public--satudora/yolo11s--832--scale-0.5--multiscale-True/predict--train2--imgsz-1664--conf-0.05/predict
 # PATH__DIR__LABEL__OUTPUT=/home/laptq/laptq-prj-21/runs/data--public--satudora/yolo11s--832--scale-0.5--multiscale-True/predict--train2--imgsz-1664--conf-0.05/predict
-PATH__DIR__IMAGE__OUTPUT=/home/laptq/laptq-prj-21/runs/RAF/train_RAF_val_RAF/predict--train_RAF_val_RAF--imgsz-832--conf-0.1/predict
-PATH__DIR__LABEL__OUTPUT=/home/laptq/laptq-prj-21/runs/RAF/train_RAF_val_RAF/predict--train_RAF_val_RAF--imgsz-832--conf-0.1/predict
+# PATH__DIR__IMAGE__OUTPUT=/home/laptq/laptq-prj-21/runs/RAF/train_RAF_val_RAF/predict--train_RAF_val_RAF--imgsz-832--conf-0.1/predict
+# PATH__DIR__LABEL__OUTPUT=/home/laptq/laptq-prj-21/runs/RAF/train_RAF_val_RAF/predict--train_RAF_val_RAF--imgsz-832--conf-0.1/predict
+PATH__DIR__IMAGE__OUTPUT=/home/laptq/laptq-prj-21/runs/data--synthetic--satudora-center-box/yolo11s--832--scale-0.5--multiscale-True/predict--train--imgsz-832--conf-0.1/predict
+PATH__DIR__LABEL__OUTPUT=/home/laptq/laptq-prj-21/runs/data--synthetic--satudora-center-box/yolo11s--832--scale-0.5--multiscale-True/predict--train--imgsz-832--conf-0.1/predict
+
 
 declare -A MAP__NAME_VIDEO__TO__=(
     # ["1_2024-11-26_081159_0_5min.mp4"]=""
