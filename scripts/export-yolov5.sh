377cb4551e15e4d633dcac5e2a32f7c34775fbfa
LapTQ <lap.tq4@gmail.com>
2025-01-13 17:58:13 +0700

    msg

--- scripts/export-yolov5.sh
+++ scripts/export-yolov5.sh
@@ -1,5 +1,3 @@
-
-
 cd submodules/yolov5
 python3 export.py \
     --weights /mnt/ssd8tb/shared_workspace/laptq/laptq-prj-21/runs/data--synthetic--syn-text2image-satudora-center--satudora-center-box--paste-not-person/yolov5s--832--scale-0.5--multiscale-True/exp/weights/best.pt \
