e515f1485a5b90e2b2c7c7ebdcbe5045d8bc63f8
LapTQ <lap.tq4@gmail.com>
2025-02-12 15:38:51 +0700

    msg

--- scripts/export-yolo.sh
+++ scripts/export-yolo.sh
@@ -1,7 +1,7 @@
 
 
 yolo export \
-    model=/mnt/ssd8tb/shared_workspace/laptq/laptq-prj-44/runs/product-person/yolo11m--640--weighted-fitness/train/weights/best.pt \
+    model=/mnt/ssd8tb/shared_workspace/laptq/laptq-prj-44/runs/product-person--public--Satudora-finetune/yolo11m--640--weighted-fitness/train/weights/best.pt \
     imgsz=640 \
     format=onnx \
     dynamic=True \
