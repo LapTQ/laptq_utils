3ed50d18cdd763185e8ab6e834d43d503049b99e
LapTQ <lap.tq4@gmail.com>
2025-02-20 15:19:57 +0700

    msg

--- scripts/helper--extract--ultralytics--detect--video.sh
+++ scripts/helper--extract--ultralytics--detect--video.sh
@@ -1,22 +1,30 @@
-PATH__DIR__VIDEO=/home/laptq/Downloads/videos
+PATH__DIR__VIDEO=/home/laptq/laptq-prj-21/data/video-20250220
 
-PATH__FILE__MODEL=/home/laptq/Downloads/yolo11n.pt
+TO_USE__YOLOv5_COMPAT=True
+PATH__FILE__MODEL=/home/laptq/laptq-prj-21/runs/data--synthetic--syn-text2image-satudora-center--satudora-center-box--paste-not-person/yolov5s--832--scale-0.5--multiscale-True/exp/weights/best.pt
+ID__DATA=data--synthetic--syn-text2image-satudora-center--satudora-center-box--paste-not-person
+ID__MODEL=yolov5s--832--scale-0.5--multiscale-True--exp
+ID__TRAIN=exp
 
-PATH__DIR__IMAGE__OUTPUT=/home/laptq/Downloads/outputs--video--1
-POSTFIX__DIR__IMAGE__OUTPUT=""
-PATH__DIR__LABEL__OUTPUT=/home/laptq/Downloads/outputs--video--1
-POSTFIX__DIR__LABEL__OUTPUT=""
+IMGSZ=832
+THRESH__CONF__MIN=0.01
+ID__PREDICT=imgsz-$IMGSZ--conf-$THRESH__CONF__MIN
+
+DEVICE="cuda:0"
+NUM__PAD__0=9
+
+PATH__DIR__LABEL__OUTPUT=/home/laptq/laptq-prj-21/outputs/20250220--labels
+POSTFIX__DIR__LABEL__OUTPUT="--PRED--DATA--${ID__DATA}--MODEL--${ID__MODEL}--EXP--${ID__TRAIN}--PREDICT--${ID__PREDICT}--json"
+
 
 declare -A MAP__SUBPATH_VIDEO__TO__=(
-    # ["1_2024-11-26_081159_0_5min.mp4"]=""
-    # ["1_2024-11-26_081159_1_5min.mp4"]=""
-    # ["1_2024-11-26_081159_2_5min.mp4"]=""
-    # ["1_2024-11-26_081159_3_5min.mp4"]=""
+    # ["1_2024-11-26_081159_0.mp4"]=""
+    # ["1_2024-11-26_081159_1.mp4"]=""
+    # ["1_2024-11-26_081159_2.mp4"]=""
+    # ["1_2024-11-26_081159_3.mp4"]=""
 
-    ["1_2024-11-26_081159_0.mp4"]=""
-    ["1_2024-11-26_081159_1.mp4"]=""
-    ["1_2024-11-26_081159_2.mp4"]=""
-    ["1_2024-11-26_081159_3.mp4"]=""
+    ["Camera_４８/Camera_48_1_2025-01-30_000000.3gp"]=""
+    ["Camera_４８/Camera_48_1_2025-01-31_000000.3gp"]=""
 )
 
 IFS=$'\n'
@@ -36,18 +44,18 @@ for subpath__video in "${!MAP__SUBPATH_VIDEO__TO__[@]}"; do
     mkdir -p "${path__dir__img__output}"
     mkdir -p "${path__dir__lbl__output}"
 
-    python3 main.py \
+    python3 submodules/laptq_utils/main.py \
         helper__extract__ultralytics__detect__video \
         --path__file__input "${path__file__input}" \
         --path__dir__img__output "${path__dir__img__output}" \
         --path__dir__lbl__output "${path__dir__lbl__output}" \
         --path__file__model "${PATH__FILE__MODEL}" \
-        --device "cuda:0" \
-        --imgsz 640 \
-        --thresh__conf__min 0.01 \
+        --device $DEVICE \
+        --imgsz $IMGSZ \
+        --thresh__conf__min $THRESH__CONF__MIN \
         --to_save__img False \
-        --num__pad__0 6 \
-        --to_use__yolov5_compat True
+        --num__pad__0 $NUM__PAD__0 \
+        --to_use__yolov5_compat $TO_USE__YOLOv5_COMPAT
 
     echo -e "${TAG__INFO} Done: ${subpath__video}"
         
