b1c6f0c35f3333d7822dd76f3ebc942a85c65432
LapTQ <lap.tq4@gmail.com>
2025-02-20 17:34:08 +0700

    msg

--- scripts/helper--convert--images--to--video.sh
+++ scripts/helper--convert--images--to--video.sh
@@ -1,9 +1,13 @@
-PATH__DIR__IMAGE__INPUT=/home/laptq/laptq-prj-21/runs/data--synthetic/yolo11s--832--scale-0.5--multiscale-True/predict--train--imgsz-832--conf-0.1/predict/draw--images
+PATH__DIR__IMAGE__INPUT=/home/laptq/laptq-prj-21/outputs/20250220--visualized
+POSTFIX__DIR__IMAGE="--PRED--DATA--data--synthetic--syn-text2image-satudora-center--satudora-center-box--paste-not-person--MODEL--yolov5s--832--scale-0.5--multiscale-True--TRAIN--train--PREDICT--imgsz-832--conf-0.1--JSON"
 
-PATH__DIR__VIDEO__OUTPUT=/home/laptq/laptq-prj-21/runs/data--synthetic/yolo11s--832--scale-0.5--multiscale-True/predict--train--imgsz-832--conf-0.1/predict/draw--images--to--video
+PATH__DIR__VIDEO__OUTPUT=/home/laptq/laptq-prj-21/outputs/20250220--frames-to-video-2
 
 declare -A MAP__NAME_VIDEO__TO__=(
-    ["1_2024-11-26_081159_0_30s.mp4"]=""
+    ["1_2024-11-26_081159_0.mp4"]=""
+    ["1_2024-11-26_081159_1.mp4"]=""
+    ["1_2024-11-26_081159_2.mp4"]=""
+    ["1_2024-11-26_081159_3.mp4"]=""
 )
 
 # [[ -d "${PATH__DIR__VIDEO__OUTPUT}" ]] && rm -r "${PATH__DIR__VIDEO__OUTPUT}"
@@ -17,7 +21,7 @@ TAG__WARNING="\033[33m[WARNING]\033[0m"
 
 
 for name__video in "${!MAP__NAME_VIDEO__TO__[@]}"; do
-    path__dir__img__input="${PATH__DIR__IMAGE__INPUT}/${name__video}"
+    path__dir__img__input="${PATH__DIR__IMAGE__INPUT}/${name__video}/vis${POSTFIX__DIR__IMAGE}"
     path__file__output="${PATH__DIR__VIDEO__OUTPUT}/${name__video}"
 
     ffmpeg \
