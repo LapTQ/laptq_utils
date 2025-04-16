a059857aaca46694a4ab74fc444d19c84c878257
LapTQ <lap.tq4@gmail.com>
2025-04-16 13:24:42 +0900

    .

--- scripts/test-yolo.sh
+++ scripts/test-yolo.sh
@@ -1,12 +1,19 @@
-path__dir__run=/home/laptq/laptq-nedo-fed/runs
+# path__dir__run=/home/laptq/laptq-nedo-fed/runs
+path__dir__run=/home/lap_awlv/laptq-nedo-fed/runs
 
 # data_val=data--c1-d19
 # data_val=data--c2-d20
-data_val=data--c1-d19--c2-d20
+# data_val=data--c1-d19--c2-d20
+# data_val=data--c1-d19-splitv2
+data_val=data--c2-d20-splitv2
+# data_val=data--c1-d19-splitv2--c2-d20-splitv2
 
 # data=data--c1-d19
 # data=data--c2-d20
-data=data--c1-d19--c2-d20
+# data=data--c1-d19--c2-d20
+data=data--c1-d19-splitv2
+# data=data--c2-d20-splitv2
+# data=data--c1-d19-splitv2--c2-d20-splitv2
 
 imgsz=640
 
@@ -17,13 +24,13 @@ ver__train=train
 yolo val \
     data=src/configs/$data_val.yaml \
     imgsz=$imgsz \
-    device=0 \
+    device=1 \
     batch=64 \
-    model=$path__dir__run/$data/${ver__model}/$ver__train/weights/best.pt \
-    project=$path__dir__run/$data/${ver__model}/val--$ver__train--imgsz-$imgsz \
+    model=/home/lap_awlv/laptq-nedo-fed/outputs/create-global-model-backward-update-on-epoch/MODEL-0--DATA_ID-data--c1-d19-splitv2--MODEL_ID-yolov8s--640--TRAIN_ID-train-fedavg-NUM_EPOCH-15x8-LR0-0.01--MODEL-1--DATA_ID-data--c2-d20-splitv2--MODEL_ID-yolov8s--640--TRAIN_ID-train-fedavg-NUM_EPOCH-15x8-LR0-0.01--METHOD-avg/epoch112.pt \
+    project=/home/lap_awlv/laptq-nedo-fed/outputs/trivials \
 
-    # model=/home/lap_awlv/fed-object-detection/outputs/soup/yolov8s_model_soup.pt \
-    # project=$path__dir__run/soup/yolov8s_model_soup/val--imgsz-$imgsz \
+    # model=$path__dir__run/$data/${ver__model}/$ver__train/weights/best.pt \
+    # project=$path__dir__run/$data/${ver__model}/val--$ver__train--imgsz-$imgsz \
 
     # model=yolov8s.pt \
     # project=$path__dir__run/coco/yolov8s/val--imgsz-$imgsz \
