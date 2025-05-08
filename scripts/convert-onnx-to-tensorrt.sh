47a8e151d0b8282556aaf1067f97a9b7c3cbff3c
LapTQ <lap.tq4@gmail.com>
2025-05-08 16:33:33 +0700

    .

--- scripts/convert-onnx-to-tensorrt.sh
+++ scripts/convert-onnx-to-tensorrt.sh
@@ -5,5 +5,5 @@ python3 submodules/laptq_utils/main.py \
         --path__file__input "/mnt/hdd10tb/Users/laptq/laptq-fs26-shoplifting-detection/outputs/convert-torch-to-onnx/tsstg-hand-model-last.onnx" \
         --path__file__output "/home/laptq/laptq-fs26-shoplifting-detection/outputs/convert-onnx-to-tensorrt/tsstg-hand-model-last.trt" \
         --precision fp32 \
-        --dynamic_shape "{'input1': [(1, 2, 15, 17),(1, 2, 15, 17), (1, 2, 15, 17)], 'input2': [(1, 2, 14, 17), (1, 2, 14, 17), (1, 2, 14, 17)]}" \
+        --dynamic_shape "{'input1': [(1, 2, 15, 17),(10, 2, 15, 17), (100, 2, 15, 17)], 'input2': [(1, 2, 14, 17), (10, 2, 14, 17), (100, 2, 14, 17)]}" \
         --max_workspace_size 1 \
