c6d199d2c52c2bee079b987e7db192e4321446b1
LapTQ <lap.tq4@gmail.com>
2025-05-15 10:02:22 +0000

    .

--- scripts/test-yolo.sh
+++ scripts/test-yolo.sh
@@ -4,22 +4,22 @@ path__dir__run=/home/laptq/laptq-nedo-fed/runs
 # data_val=data--c1-d19-splitv2
 # data_val=data--c2-d20-splitv2
 # data_val=data--c1-d19-splitv2--c2-d20-splitv2
-# data_val=data--c1
+data_val=data--c1
 # data_val=data--c2
 # data_val=data--c1--c2
-data_val=data--d3.1--d3.2
+# data_val=data--d3.1--d3.2
 
 # data=data--c1-d19-splitv2
 # data=data--c2-d20-splitv2
 # data=data--c1-d19-splitv2--c2-d20-splitv2
 # data=data--c1
-# data=data--c2
+data=data--c2
 # data=data--c1--c2
 
 imgsz=640
 
 ver__model=yolov8s--640
-ver__train=train-B-E150-LR0.01
+ver__train=train-B-E150-LR0.005
 # conf=0.1
 
 yolo val \
@@ -27,10 +27,10 @@ yolo val \
     imgsz=$imgsz \
     device=0 \
     batch=128 \
-    model=/home/laptq/laptq-nedo-fed/outputs/create-global-model-backward-update-on-epoch/fedavg--c1--c2--150x1--LR0.01/epoch149.pt \
     project=/home/laptq/laptq-nedo-fed/outputs/trivials \
+    model=$path__dir__run/$data/${ver__model}/$ver__train/weights/best.pt \
+    # model=/home/laptq/laptq-nedo-fed/outputs/create-global-model-backward-update-on-epoch/fedavg--c1--c2--150x1--LR0.01/epoch149.pt \
     # project=/home/lap_awlv/laptq-nedo-fed/outputs/trivials \
-    # model=$path__dir__run/$data/${ver__model}/$ver__train/weights/best.pt \
 
     # model=$path__dir__run/$data/${ver__model}/$ver__train/weights/best.pt \
     # project=$path__dir__run/$data/${ver__model}/val--$ver__train--imgsz-$imgsz \
