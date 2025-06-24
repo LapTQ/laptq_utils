047f514e15ade2e583fcdb6c45490702a92bb44b
LapTQ <lap.tq4@gmail.com>
2025-06-24 17:59:14 +0900

    .

--- scripts/test-yolo.sh
+++ scripts/test-yolo.sh
@@ -1,9 +1,6 @@
 # path__dir__run=/home/laptq/laptq-nedo-fed/runs
 path__dir__run=/home/lap_awlv/laptq-nedo-fed/runs
 
-# data_val=data--c1-d19-splitv2
-# data_val=data--c2-d20-splitv2
-# data_val=data--c1-d19-splitv2--c2-d20-splitv2
 # data_val=data--c1
 # data_val=data--c2
 # data_val=data--c1--c2
@@ -11,12 +8,11 @@ path__dir__run=/home/lap_awlv/laptq-nedo-fed/runs
 # data_val=data--c2-night
 # data_val=data--c1-day--c2-night
 # data_val=data--d3.1--d3.2
-data_val=data--d3-satudora
+# data_val=data--d3-satudora
+data_val=data--d5.1
+# data_val=data--d5.2
 
-# data=data--c1-d19-splitv2
-# data=data--c2-d20-splitv2
-# data=data--c1-d19-splitv2--c2-d20-splitv2
-# data=data--c1
+data=data--c1
 # data=data--c2
 # data=data--c1--c2
 # data=data--c1-day
@@ -35,12 +31,12 @@ yolo val \
     device=1 \
     batch=128 \
     project=/home/lap_awlv/laptq-nedo-fed/outputs/trivials \
-    model=/home/lap_awlv/laptq-nedo-fed/outputs/create-global-model-backward-update-on-epoch/fedavg--c1-day--c2-night--150x1--LR0.01/epoch149.pt \
+    model=$path__dir__run/$data/${ver__model}/$ver__train/weights/best.pt \
+    # model=/home/lap_awlv/laptq-nedo-fed/outputs/create-global-model-backward-update-on-epoch/fedavg--c1-day--c2-night--150x1--LR0.01/epoch149.pt \
     # model=/home/lap_awlv/laptq-nedo-fed/outputs/create-global-model-backward-update-on-epoch/fedavg--c1-day--c2-night--30x5--LR0.01/epoch149.pt \
     # model=/home/lap_awlv/laptq-nedo-fed/outputs/create-global-model-backward-update-on-epoch/fedavg--c1-day--c2-night--15x10--LR0.01/epoch149.pt \
     # model=/home/lap_awlv/laptq-nedo-fed/outputs/create-global-model-backward-update-on-epoch/fedavg--c1-day--c2-night--10x15--LR0.01/epoch149.pt \
     # model=/home/lap_awlv/laptq-nedo-fed/outputs/create-global-model-backward-update-on-epoch/fedavg--c1-day--c2-night--5x30--LR0.01/epoch149.pt \
-    # model=$path__dir__run/$data/${ver__model}/$ver__train/weights/best.pt \
     # model=/home/lap_awlv/laptq-nedo-fed/outputs/create-global-model-backward-update-on-epoch/fedavg--c1--c2--5x30--LR0.01/epoch120.pt \
     # model=/home/lap_awlv/laptq-nedo-fed/outputs/create-global-model-backward-update-on-epoch/fedavg--c1--c2--10x15--LR0.01/epoch135.pt \
     # model=/home/lap_awlv/laptq-nedo-fed/outputs/create-global-model-backward-update-on-epoch/fedavg--c1--c2--15x10--LR0.01/epoch140.pt \
