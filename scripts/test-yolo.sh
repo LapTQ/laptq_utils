f88e4489a62e422f1f995cda04284448ebedf041
LapTQ <lap.tq4@gmail.com>
2025-07-07 11:59:52 +0900

    .

--- scripts/test-yolo.sh
+++ scripts/test-yolo.sh
@@ -1,25 +1,42 @@
-path__dir__run=/mnt/hdd10tb/Users/laptq/laptq-prj-46/runs
-
-data=data--public--satudora
-data_val=data--testset-4cam-factory
+# path__dir__run=/home/laptq/laptq-nedo-fed/runs
+path__dir__run=/home/lap_awlv/laptq-nedo-fed/runs
 
 
-ver__model=yolo11s--832--scale-0.5--multiscale-True
-imgsz=832
+LS__DATA_VAL=(
+    data--c1
+    data--c2
+)
 
-ver__train=train2
-conf=0.05
+data=data--c1
+# data=data--c2
 
-yolo val \
-    data=src/configs/$data_val.yaml \
-    model=$path__dir__run/$data/${ver__model}/$ver__train/weights/best.pt \
-    project=$path__dir__run/$data/${ver__model}/val--$ver__train--imgsz-$imgsz--conf-$conf \
-    imgsz=$imgsz \
-    conf=$conf \
-    iou=0.5 \
-    device=0 \
-    batch=8
 
-    
-    # model=/mnt/ssd8tb/shared_workspace/manhpc/FS_prj21/runs/train_RAF_val_RAF/weights/best.pt \
-    # project=$path__dir__run/RAF/train_RAF_val_RAF/val--train_RAF_val_RAF--imgsz-$imgsz--conf-$conf \
+imgsz=640
+
+ver__model=yolov8s--640
+ver__train=train-B-E150-LR0.01
+# conf=0.1
+
+for data_val in ${LS__DATA_VAL[@]}; do
+    echo
+    echo "========= Evaluating data $data_val ========="
+
+    yolo val \
+        data=src/configs/$data_val.yaml \
+        imgsz=$imgsz \
+        device=0 \
+        batch=128 \
+        project=/home/lap_awlv/laptq-nedo-fed/outputs/trivials \
+        model=$path__dir__run/$data/${ver__model}/$ver__train/weights/best.pt \
+        # model=yolov8s.pt \
+        
+        # project=/home/laptq/laptq-nedo-fed/outputs/trivials \
+        # project=$path__dir__run/$data/${ver__model}/val--$ver__train--imgsz-$imgsz \
+        # project=$path__dir__run/$data/${ver__model}/val--$ver__train--imgsz-$imgsz--conf-$conf \
+        # project=$path__dir__run/coco/yolov8s/val--imgsz-$imgsz \
+
+
+        # conf=$conf \
+        # iou=0.6 \
+
+done
