e505967a10f6ad83a062927a7b76b93e024c50a4
LapTQ <lap.tq4@gmail.com>
2025-07-07 12:09:26 +0900

    .

--- scripts/test-yolo.sh
+++ scripts/test-yolo.sh
@@ -9,18 +9,23 @@ LS__DATA_VAL=(
     # data--c1-day
     # data--c2-night
     # data--c1-day--c2-night
-    # data--d3.1--d3.2
+    data--d3.1--d3.2
     # data--d3-satudora
-    # data--d5.1
+    data--d5.1
     data--d5.2
+    data--d5.1-recon
+    data--d5.2-recon
 )
 
-data=data--c1
+# data=data--c1
 # data=data--c2
 # data=data--c1--c2
 # data=data--c1-day
 # data=data--c2-night
 # data=data--c1-day--c2-night
+# data=data--c1-recon
+# data=data--c2-recon
+data=data--c1-recon--c2-recon
 
 
 imgsz=640
@@ -28,6 +33,7 @@ imgsz=640
 ver__model=yolov8s--640
 ver__train=train-B-E150-LR0.01
 # conf=0.1
+
 
 for data_val in ${LS__DATA_VAL[@]}; do
     echo
@@ -39,7 +45,8 @@ for data_val in ${LS__DATA_VAL[@]}; do
         device=0 \
         batch=128 \
         project=/home/lap_awlv/laptq-nedo-fed/outputs/trivials \
-        model=/home/lap_awlv/laptq-nedo-fed/outputs/create-global-model-backward-update-on-epoch/fedavg--c1--c2--150x1--LR0.01/epoch149.pt \
+        model=/home/lap_awlv/laptq-nedo-fed/outputs/create-global-model-backward-update-on-epoch/fedavg--c1-recon--c2-recon--150x1--LR0.01/epoch149.pt \
+        # model=/home/lap_awlv/laptq-nedo-fed/outputs/create-global-model-backward-update-on-epoch/fedavg--c1--c2--150x1--LR0.01/epoch149.pt \
         # model=/home/lap_awlv/laptq-nedo-fed/outputs/create-global-model-backward-update-on-epoch/fedavg--c1--c2--30x5--LR0.01/epoch145.pt \
         # model=/home/lap_awlv/laptq-nedo-fed/outputs/create-global-model-backward-update-on-epoch/fedavg--c1--c2--15x10--LR0.01/epoch140.pt \
         # model=/home/lap_awlv/laptq-nedo-fed/outputs/create-global-model-backward-update-on-epoch/fedavg--c1--c2--10x15--LR0.01/epoch135.pt \
