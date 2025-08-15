a27124bee18bbcb80e086f10de1d39e64a2fdd05
LapTQ <lap.tq4@gmail.com>
2025-08-15 10:43:00 +0900

    .

--- scripts/test-yolo.sh
+++ scripts/test-yolo.sh
@@ -9,12 +9,14 @@ LS__DATA_VAL=(
     # data--c1-day
     # data--c2-night
     # data--c1-day--c2-night
-    data--d3.1--d3.2
+    # data--d3.1--d3.2
     # data--d3-satudora
-    data--d5.1
-    data--d5.2
-    data--d5.1-recon
-    data--d5.2-recon
+    # data--d5.1
+    # data--d5.2
+    # data--d5.1-recon
+    # data--d5.2-recon
+    data--d5.1--d5.2
+    # data--d7.1--d7.2
 )
 
 # data=data--c1
@@ -26,7 +28,7 @@ LS__DATA_VAL=(
 # data=data--c1-recon
 # data=data--c2-recon
 # data=data--c1-recon--c2-recon
-data=data--c1--c2--c1-recon--c2-recon
+# data=data--c1--c2--c1-recon--c2-recon
 
 
 imgsz=640
@@ -43,22 +45,22 @@ for data_val in ${LS__DATA_VAL[@]}; do
     yolo val \
         data=src/configs/$data_val.yaml \
         imgsz=$imgsz \
-        device=1 \
-        batch=128 \
+        device=2 \
+        batch=512 \
         project=/home/lap_awlv/laptq-nedo-fed/outputs/trivials \
-        model=/home/lap_awlv/laptq-nedo-fed/outputs/create-global-model-backward-update-on-epoch/fedavg--c1--c2--c1-recon--c2-recon--150x1--LR0.01/epoch149.pt \
-        # model=/home/lap_awlv/laptq-nedo-fed/outputs/create-global-model-backward-update-on-epoch/fedavg--c1-recon--c2-recon--150x1--LR0.01/epoch149.pt \
-        # model=/home/lap_awlv/laptq-nedo-fed/outputs/create-global-model-backward-update-on-epoch/fedavg--c1--c2--150x1--LR0.01/epoch149.pt \
-        # model=/home/lap_awlv/laptq-nedo-fed/outputs/create-global-model-backward-update-on-epoch/fedavg--c1--c2--30x5--LR0.01/epoch145.pt \
-        # model=/home/lap_awlv/laptq-nedo-fed/outputs/create-global-model-backward-update-on-epoch/fedavg--c1--c2--15x10--LR0.01/epoch140.pt \
-        # model=/home/lap_awlv/laptq-nedo-fed/outputs/create-global-model-backward-update-on-epoch/fedavg--c1--c2--10x15--LR0.01/epoch135.pt \
-        # model=/home/lap_awlv/laptq-nedo-fed/outputs/create-global-model-backward-update-on-epoch/fedavg--c1--c2--5x30--LR0.01/epoch120.pt \
-        # model=/home/lap_awlv/laptq-nedo-fed/outputs/create-global-model-backward-update-on-epoch/fedavg--c1-day--c2-night--150x1--LR0.01/epoch149.pt \
-        # model=/home/lap_awlv/laptq-nedo-fed/outputs/create-global-model-backward-update-on-epoch/fedavg--c1-day--c2-night--30x5--LR0.01/epoch149.pt \
-        # model=/home/lap_awlv/laptq-nedo-fed/outputs/create-global-model-backward-update-on-epoch/fedavg--c1-day--c2-night--15x10--LR0.01/epoch149.pt \
-        # model=/home/lap_awlv/laptq-nedo-fed/outputs/create-global-model-backward-update-on-epoch/fedavg--c1-day--c2-night--10x15--LR0.01/epoch149.pt \
-        # model=/home/lap_awlv/laptq-nedo-fed/outputs/create-global-model-backward-update-on-epoch/fedavg--c1-day--c2-night--5x30--LR0.01/epoch149.pt \
+        model=/home/lap_awlv/laptq-nedo-fed/outputs/create-global-model-backward-update-on-epoch/yolov8s--640/fedavg--c1--c2--150x1--LR0.01/epoch149.pt \
         # model=$path__dir__run/$data/${ver__model}/$ver__train/weights/best.pt \
+        # model=/home/lap_awlv/laptq-nedo-fed/outputs/create-global-model-backward-update-on-epoch/yolov8s--640/fedavg--c1--c2--c1-recon--c2-recon--150x1--LR0.01/epoch149.pt \
+        # model=/home/lap_awlv/laptq-nedo-fed/outputs/create-global-model-backward-update-on-epoch/yolov8s--640/fedavg--c1-recon--c2-recon--150x1--LR0.01/epoch149.pt \
+        # model=/home/lap_awlv/laptq-nedo-fed/outputs/create-global-model-backward-update-on-epoch/yolov8s--640/fedavg--c1--c2--30x5--LR0.01/epoch145.pt \
+        # model=/home/lap_awlv/laptq-nedo-fed/outputs/create-global-model-backward-update-on-epoch/yolov8s--640/fedavg--c1--c2--15x10--LR0.01/epoch140.pt \
+        # model=/home/lap_awlv/laptq-nedo-fed/outputs/create-global-model-backward-update-on-epoch/yolov8s--640/fedavg--c1--c2--10x15--LR0.01/epoch135.pt \
+        # model=/home/lap_awlv/laptq-nedo-fed/outputs/create-global-model-backward-update-on-epoch/yolov8s--640/fedavg--c1--c2--5x30--LR0.01/epoch120.pt \
+        # model=/home/lap_awlv/laptq-nedo-fed/outputs/create-global-model-backward-update-on-epoch/yolov8s--640/fedavg--c1-day--c2-night--150x1--LR0.01/epoch149.pt \
+        # model=/home/lap_awlv/laptq-nedo-fed/outputs/create-global-model-backward-update-on-epoch/yolov8s--640/fedavg--c1-day--c2-night--30x5--LR0.01/epoch149.pt \
+        # model=/home/lap_awlv/laptq-nedo-fed/outputs/create-global-model-backward-update-on-epoch/yolov8s--640/fedavg--c1-day--c2-night--15x10--LR0.01/epoch149.pt \
+        # model=/home/lap_awlv/laptq-nedo-fed/outputs/create-global-model-backward-update-on-epoch/yolov8s--640/fedavg--c1-day--c2-night--10x15--LR0.01/epoch149.pt \
+        # model=/home/lap_awlv/laptq-nedo-fed/outputs/create-global-model-backward-update-on-epoch/yolov8s--640/fedavg--c1-day--c2-night--5x30--LR0.01/epoch149.pt \
         # model=yolov8s.pt \
         
         # project=/home/laptq/laptq-nedo-fed/outputs/trivials \
