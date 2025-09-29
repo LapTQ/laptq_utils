4840ea5b87e552e4ab720dd077a5029d6c6de0a6
LapTQ <lap.tq4@gmail.com>
2025-09-29 10:15:42 +0700

    .

--- scripts/helper--convert--video--to--images.py
+++ scripts/helper--convert--video--to--images.py
@@ -13,7 +13,7 @@ import glob
 
 MAP__SUBPATH_VIDEO__TO__ = {
     p[len(PATH__DIR__VIDEO) + 1 :]: None
-    for p in glob.glob(f"{PATH__DIR__VIDEO}/shoplifting-awlrecord-videos/*/*.mp4")
+    for p in glob.glob(f"{PATH__DIR__VIDEO}/shoplifting-gen-videos/veo3/v1/*.mp4")
     if os.path.isfile(p)
 }
 
